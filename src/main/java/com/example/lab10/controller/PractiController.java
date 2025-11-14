302140b442607ce08f17d575ded5b666c303d0a9
Sergio <a20223291@pucp.edu.pe>
2025-11-13 21:43:03 -0500

    commit 2

--- src/main/java/com/example/lab10/controller/PractiController.java
+++ src/main/java/com/example/lab10/controller/PractiController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/practicantes/")
+@RequestMapping("/practicantes")
 public class PractiController {
 
     @GetMapping
